7d6acbfbd460de8294d66929d13b0d0e49c63965
0xDVC <neilohene@gmail.com>
2025-05-12 22:51:19 +0000

    completed scanner

--- main.go
+++ main.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 type Lox struct {
-    hadError bool
+	hadError bool
 }
 
 var vm = Lox{}
 
 func main() {
-    if len(os.Args) > 2 {
-        fmt.Println("Usage: golox [script]")
-        os.Exit(64)
-    }
-    if len(os.Args) == 2 {
-        vm.runFile(os.Args[1])
-    }else {
-        vm.runPrompt()
-    }
+	if len(os.Args) > 2 {
+		fmt.Println("Usage: golox [script]")
+		os.Exit(64)
+	}
+	if len(os.Args) == 2 {
+		vm.runFile(os.Args[1])
+	} else {
+		vm.runPrompt()
+	}
 }
 
 func (l *Lox) runFile(path string) error {
-    bytes, err := os.ReadFile(path)
-    if err != nil {
-        return err
-    }
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
 
-    l.run(string(bytes))
-    if l.hadError {
-        os.Exit(65)
-    }
-    return nil
+	l.run(string(bytes))
+	if l.hadError {
+		os.Exit(65)
+	}
+	return nil
 }
 
 func (l *Lox) runPrompt() {
-    input := bufio.NewScanner(os.Stdin)
+	input := bufio.NewScanner(os.Stdin)
 
-    for {
-        fmt.Print("> ")
-        if ok := input.Scan(); !ok {
-            break
-        }
-        line := input.Text()
-        l.run(line)
-        l.hadError = false
-    }  
+	for {
+		fmt.Print("> ")
+		if ok := input.Scan(); !ok {
+			break
+		}
+		line := input.Text()
+		l.run(line)
+		l.hadError = false
+	}
 }
 
 func (l *Lox) run(source string) {
-    scanner := NewScanner(source)
-    scanner.scanTokens()
-    
-    for _, token := range scanner.tokens {
-        fmt.Println(token)
-    }
+	scanner := NewScanner(source)
+	scanner.scanTokens()
+
+	for _, token := range scanner.tokens {
+		fmt.Println(token)
+	}
 }
 
 func (l *Lox) reportError(line int, msg string) {
-    l.report(line, "", msg)
-} 
+	l.report(line, "", msg)
+}
 
 func (l *Lox) report(line int, where, msg string) {
-    fmt.Printf("[line %d] Error %s: %s\n", line, where, msg)
-    l.hadError  = true
-} 
+	fmt.Printf("[line %d] Error %s: %s\n", line, where, msg)
+	l.hadError = true
+}
--- scanner.go
+++ scanner.go
@@ -1,144 +1,208 @@
 package main
 
-type Scanner struct {
-    source []rune
-    tokens  []*Token
+import (
+	"strconv"
+)
 
-    start int
-    current int
-    line int
+type Scanner struct {
+	source []rune
+	tokens []*Token
+
+	start   int
+	current int
+	line    int
 }
 
 func NewScanner(source string) *Scanner {
-    return &Scanner{
-        source: []rune(source), 
-        line: 1,
-    }
+	return &Scanner{
+		source: []rune(source),
+		line:   1,
+	}
 }
 
 func (s *Scanner) scanTokens() {
-    for !s.isAtEnd() {
-        s.start = s.current
-        s.scanToken()
-    }
+	for !s.isAtEnd() {
+		s.start = s.current
+		s.scanToken()
+	}
 
-    s.tokens = append(s.tokens, &Token{
-        TokenType: TokenType_EOF,
-        Lexeme: "",
-        Literal: nil,
-        Line: s.line,
-    })
+	s.tokens = append(s.tokens, &Token{
+		TokenType: TokenType_EOF,
+		Lexeme:    "",
+		Literal:   nil,
+		Line:      s.line,
+	})
 }
 
 func (s *Scanner) isAtEnd() bool {
-    return s.current >= len(s.source)
+	return s.current >= len(s.source)
 }
 
-
 func (s *Scanner) scanToken() {
-    var c rune = s.advance()
-    
-    switch c {
-        case '(':
-            s.addToken(TokenType_LeftParen)
-        case ')':
-            s.addToken(TokenType_RightParen)
-        case '{':
-            s.addToken(TokenType_LeftBrace)
-        case '}':
-            s.addToken(TokenType_RightBrace)
-        case ',':
-            s.addToken(TokenType_Comma)
-        case '.':
-            s.addToken(TokenType_Dot)
-        case '-':
-            s.addToken(TokenType_Minus)
-        case '+':
-            s.addToken(TokenType_Plus)
-        case ';':
-            s.addToken(TokenType_Semicolon)
-        case '*':
-            s.addToken(TokenType_Star)
-        case '!':
-            if s.match('=') {
-                s.addToken(TokenType_BangEqual)
-            } else {
-                s.addToken(TokenType_Bang)
-            }
-        case '=':
-            if s.match('=') {
-                s.addToken(TokenType_EqualEqual)
-            } else {
-                s.addToken(TokenType_Equal)
-            }
-        case '<':
-            if s.match('=') {
-                s.addToken(TokenType_LessEqual)
-            } else {
-                s.addToken(TokenType_Less)
-            }
-        case '>':
-            if s.match('=') {
-                s.addToken(TokenType_GreaterEqual)
-            } else {
-                s.addToken(TokenType_Greater)
-            }
-        case '/':
-            // If you encouter two slashes in a row, consume a comment until the end of the line
-            if s.match('/') {
-                for s.peek() != '\n' && !s.isAtEnd() {
-                    s.advance()
-                }
-            } else {
-                s.addToken(TokenType_Slash)
-            }
-        case ' ', '\r', '\t':
-            // TODO do we actually need it?
-            break
-        case '\n':
-            s.line++
+	var c rune = s.advance()
 
-        default:
-            vm.reportError(s.line, "Unexpected character.")
-    }
+	switch c {
+	case '(':
+		s.addToken(TokenType_LeftParen)
+	case ')':
+		s.addToken(TokenType_RightParen)
+	case '{':
+		s.addToken(TokenType_LeftBrace)
+	case '}':
+		s.addToken(TokenType_RightBrace)
+	case ',':
+		s.addToken(TokenType_Comma)
+	case '.':
+		s.addToken(TokenType_Dot)
+	case '-':
+		s.addToken(TokenType_Minus)
+	case '+':
+		s.addToken(TokenType_Plus)
+	case ';':
+		s.addToken(TokenType_Semicolon)
+	case '*':
+		s.addToken(TokenType_Star)
+	case '!':
+		if s.match('=') {
+			s.addToken(TokenType_BangEqual)
+		} else {
+			s.addToken(TokenType_Bang)
+		}
+	case '=':
+		if s.match('=') {
+			s.addToken(TokenType_EqualEqual)
+		} else {
+			s.addToken(TokenType_Equal)
+		}
+	case '<':
+		if s.match('=') {
+			s.addToken(TokenType_LessEqual)
+		} else {
+			s.addToken(TokenType_Less)
+		}
+	case '>':
+		if s.match('=') {
+			s.addToken(TokenType_GreaterEqual)
+		} else {
+			s.addToken(TokenType_Greater)
+		}
+	case '/':
+		// If you encouter two slashes in a row, consume a comment until the end of the line
+		if s.match('/') {
+			for s.peek() != '\n' && !s.isAtEnd() {
+				s.advance()
+			}
+		} else {
+			s.addToken(TokenType_Slash)
+		}
+	case ' ', '\r', '\t':
+		// TODO do we actually need it?
+		break
+	case '\n':
+		s.line++
+
+	case '"':
+		s.scanString()
+
+	default:
+		if c >= '0' && c <= '9' {
+			s.scanNumber()
+		} else {
+			vm.reportError(s.line, "Unexpected character.")
+		}
+	}
 }
 
 func (s *Scanner) advance() rune {
-    result := s.source[s.current]
-    s.current++
-    return result
+	result := s.source[s.current]
+	s.current++
+	return result
 }
 
 func (s *Scanner) addToken(t TokenType) {
-    s.addTokenWithLiteral(t, nil)
+	s.addTokenWithLiteral(t, nil)
 }
 
 func (s *Scanner) addTokenWithLiteral(t TokenType, literal any) {
-    text := string(s.source[s.start:s.current])
-    s.tokens = append(s.tokens, &Token {
-        TokenType: t, 
-        Lexeme: text,
-        Literal: literal, 
-        Line: s.line,
-    })
+	text := string(s.source[s.start:s.current])
+	s.tokens = append(s.tokens, &Token{
+		TokenType: t,
+		Lexeme:    text,
+		Literal:   literal,
+		Line:      s.line,
+	})
 }
 
 func (s *Scanner) match(expected rune) bool {
-    if (s.isAtEnd()) {
-        return false
-    }
+	if s.isAtEnd() {
+		return false
+	}
 
-    if s.source[s.current] != expected {
-        return false
-    }
+	if s.source[s.current] != expected {
+		return false
+	}
 
-    s.current++
-    return true
+	s.current++
+	return true
 }
 
 func (s *Scanner) peek() rune {
-    if s.isAtEnd() {
-        return 0
-    }
-    return s.source[s.current]
+	if s.isAtEnd() {
+		return 0
+	}
+	return s.source[s.current]
+}
+
+func (s *Scanner) scanString() {
+	for s.peek() != '"' && !s.isAtEnd() {
+		if s.peek() == '\n' {
+			s.line++
+		}
+		s.advance()
+	}
+
+	if s.isAtEnd() {
+		vm.reportError(s.line, "Unterminated string.")
+	}
+
+	s.advance()
+
+	value := string(s.source[s.start+1 : s.current-1])
+	s.addTokenWithLiteral(TokenType_String, value)
+}
+
+func isDigit(c rune) bool {
+	return c >= '0' && c <= '9'
+}
+
+func (s *Scanner) scanNumber() {
+	for isDigit(s.peek()) {
+		s.advance()
+	}
+
+	if s.peek() == '.' && isDigit(s.peekNext()) {
+		//consume the dot
+		s.advance()
+
+		for isDigit(s.peek()) {
+			s.advance()
+		}
+	}
+
+	// TODO: Report a parsing error if it occurs
+	text := string(s.source[s.start:s.current])
+	num, err := strconv.ParseFloat(text, 64)
+	if err != nil {
+		//TODO: use the built-in erro interface instead of plain string literals
+		vm.reportError(s.line, "Invalid number literal.")
+		return
+	}
+	s.addTokenWithLiteral(TokenType_Number, num)
+}
+
+func (s *Scanner) peekNext() rune {
+	if s.current+1 >= len(s.source) {
+		return 0
+	}
+	return s.source[s.current+1]
 }
