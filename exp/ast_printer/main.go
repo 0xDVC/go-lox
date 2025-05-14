e63cfd136f7e8787fca7b6bb7e2b2950aed32e62
0xDVC <neilohene@gmail.com>
2025-05-14 09:27:44 +0000

    feat: implement test for AstPrinter

---
+++ exp/ast_printer/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/0xdvc/go-lox/expr"
+	"github.com/0xdvc/go-lox/token"
+)
+
+func main() {
+	expression := expr.Binary[string]{
+		Left: expr.Unary[string]{
+			Operator: token.NewToken(token.TokenType_Minus, "-", nil, 1),
+			Right:    expr.Literal[string]{Value: 123},
+		},
+		Operator: token.NewToken(token.TokenType_Star, "*", nil, 1),
+		Right: expr.Grouping[string]{
+			Expression: expr.Literal[string]{Value: 45.67},
+		},
+	}
+	printer := expr.AstPrinter{}
+	fmt.Println(printer.Print(expression))
+}
